e4c90861d8bdeac6fea19360b1b92fa5e348e5b8
emma <emmaruthoneill@gmail.com>
2017-05-17 09:40:57 -0700

    fixed up the map on mobile

--- src/js/story.js
+++ src/js/story.js
@@ -257,8 +257,8 @@ var drawMap = function(key,mapDataFLAG) {
     .on("mousemove", function() {
       if (screen.width <= 480) {
         return tooltip
-          .style("top",70+"px")
-          .style("left",40+"px");
+          .style("top",(d3.event.pageY+40)+"px")//(d3.event.pageY+40)+"px")
+          .style("left",10+"px");
       } else {
         return tooltip
           .style("top", (d3.event.pageY+20)+"px")
@@ -322,305 +322,6 @@ var drawMap = function(key,mapDataFLAG) {
       }
     });
 
-
-}
-
-// function to create bar chart
-var drawBars = function() {
-
-  // show tooltip
-  var bar_tooltip = d3.select("div.tooltip-bars");
-
-  // setting sizes of interactive
-  var margin = {
-    top: 15,
-    right: 50,
-    bottom: 50,
-    left: 70
-  };
-  if (screen.width > 768) {
-    var width = 900 - margin.left - margin.right;
-    var height = 420 - margin.top - margin.bottom;
-  } else if (screen.width <= 768 && screen.width > 480) {
-    var width = 720 - margin.left - margin.right;
-    var height = 420 - margin.top - margin.bottom;
-  } else if (screen.width <= 480 && screen.width > 340) {
-    console.log("big phone");
-    var margin = {
-      top: 20,
-      right: 20,
-      bottom: 70,
-      left: 30
-    };
-    var width = 340 - margin.left - margin.right;
-    var height = 360 - margin.top - margin.bottom;
-  } else if (screen.width <= 340) {
-    console.log("mini iphone")
-    var margin = {
-      top: 20,
-      right: 20,
-      bottom: 70,
-      left: 43
-    };
-    var width = 310 - margin.left - margin.right;
-    var height = 360 - margin.top - margin.bottom;
-  }
-
-  // x-axis scale
-  var x = d3.scale.ordinal()
-      .rangeRoundBands([0, width], 0.2);
-
-  // y-axis scale
-  var y = d3.scale.linear()
-      .rangeRound([height, 0]);
-
-  x.domain(bubbleData.map(function(d) {
-    return d.short_name;
-  }));
-  y.domain(d3.extent(bubbleData, function(d) { return d.arrests; })).nice();
-
-  var xAxis = d3.svg.axis()
-      .scale(x)
-      .orient("bottom");
-      // .tickFormat(d3.time.format("%Y"));
-
-  // use y-axis scale to set y-axis
-  var yAxis = d3.svg.axis()
-      .scale(y)
-      .orient("left")
-      .tickFormat(d3.format(".2s"));
-
-  // create SVG container for chart components
-  var svgBars = d3.select(".bar-chart").append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-  svgBars.selectAll("bar")
-      .data(bubbleData)
-    .enter().append("rect")
-      .style("fill", "#6790B7")
-      .attr("x", function(d) { return x(d.short_name); })
-      .attr("width", x.rangeBand())
-      .attr("y", function(d) { return y(+d.arrests); })
-      .attr("height", function(d) {
-        return height - y(+d.arrests);
-      })
-      .on("mouseover", function(d) {
-        bar_tooltip.html(`
-            <div>Shelter: <b>${d.shelter}</b></div>
-            <div>Arrests: <b>${(d.arrests)}</b></div>
-        `);
-        bar_tooltip.style("visibility", "visible");
-      })
-      .on("mousemove", function(d) {
-        if (screen.width <= 480) {
-          return bar_tooltip
-            .style("top", (d3.event.pageY+20)+"px")
-            .style("left",d3.event.pageX/2+20+"px");
-        } else {
-          return bar_tooltip
-            .style("top", (d3.event.pageY+20)+"px")
-            .style("left",(d3.event.pageX-80)+"px");
-        }
-      })
-      .on("mouseout", function(){return bar_tooltip.style("visibility", "hidden");});
-
-  if (screen.width <= 480) {
-    svgBars.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + height + ")")
-        .call(xAxis)
-      .selectAll("text")
-          .style("text-anchor", "end")
-          .attr("dx", "-.8em")
-          .attr("dy", ".15em")
-          .attr("transform", "rotate(-65)" );
-      svgBars.append("g")
-        .attr("transform", "translate(0," + height + ")")
-        .append("text")
-        .attr("class", "label")
-        .attr("x", width)
-        .attr("y", 60)
-        .style("text-anchor", "end")
-        .text("Childrens' shelters")
-  } else {
-    svgBars.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + height + ")")
-        .call(xAxis)
-        // .selectAll("text")
-        //     .style("text-anchor", "end")
-        //     .attr("dx", "1em")
-        //     .attr("dy", "1em")
-          // .attr("transform", "rotate(-65)" )
-        .append("text")
-          .attr("class", "label")
-          .attr("x", width)
-          .attr("y", 45)
-          .style("text-anchor", "end")
-          .text("California childrens' shelters")
-  }
-
-  if (screen.width <= 480){
-    svgBars.append("g")
-        .attr("class", "y axis")
-        .call(yAxis)
-      .append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("y", 0)
-        .attr("dy", "20px")
-        .style("text-anchor", "end")
-        .text("Total arrests in 2016");
-  } else {
-    svgBars.append("g")
-        .attr("class", "y axis")
-        .call(yAxis)
-      .append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("y", 0)
-        .attr("dy", "-45px")
-        .style("text-anchor", "end")
-        .text("Total arrests in 2016");
-  };
-
-}
-
-// function to draw bubbles
-var drawBubbles = function(key,flag,highlight) {
-
-  // show tooltip
-  // var bubbles_tooltip = d3.select("div.tooltip-bubbles");
-
-  if (screen.width > 768) {
-    var diameter = 500;
-    var margin = {
-      right: 30,
-      left: 30
-    }
-    var topbuffer = 20;
-  } else if (screen.width <= 768 && screen.width > 480) {
-    var diameter = 500;
-    var margin = {
-      right: 15,
-      left: 15
-    }
-    var topbuffer = 20;
-  } else if (screen.width <= 480 && screen.width > 340) {
-    var diameter = 360;
-    var margin = {
-      right: 5,
-      left: 5
-    }
-    var topbuffer = 20;
-  } else if (screen.width <= 340) {
-    var diameter = 300;
-    var margin = {
-      right: 5,
-      left: 5
-    }
-    var topbuffer = 20;
-  }
-
-  var width = diameter-margin.left-margin.right;
-  var height = diameter-topbuffer; //because the bubbles aren't arranged so they're square
-  // var color = d3.scale.category20b();
-
-  var svg = d3.select("#"+key).append('svg')
-    .attr('width', width + margin.left + margin.right)
-    .attr('height', height)
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + 0 + ")"); // giving the bubbles some padding, so that the text won't get cut off on the right and left margins
-
-  var bubble = d3.layout.pack()
-      .sort(null)
-      // .sort(function(a, b) {
-      //   return -(a.value - b.value);
-      // }) // sorting from biggest to smallest
-      .size([width, height])
-      .padding(2)
-      .value(function(d){
-        if (flag == 1) {
-          if (d.call_types == "Runaway") {
-            return 0;
-          }
-        }
-        return +d.call_totals;
-      });
-
-  // adding the nodes to the chart (automatically generate attributes)
-  var nodes = bubble.nodes({children: callsData})
-    .filter(function(d) { return !d.children; }); // filter out the outer bubble
-
-  var node = svg.selectAll('.node')
-    .data(nodes, d => d.call_types);
-
-  //setup the chart
-  var bubbles = svg.append("g")
-      .attr("transform", "translate(0,0)")
-      .selectAll(".bubble")
-      .data(nodes)
-      .enter()
-      .append("g");
-      // .on("mouseover", function(d) {
-      //   bubbles_tooltip.html(`
-      //       <div>Call type: <b>${d.call_types}</b></div>
-      //       <div>Total calls: <b>${formatthousands(d.call_totals)}</b></div>
-      //   `);
-      //   bubbles_tooltip.style("visibility", "visible");
-      // })
-      // .on("mousemove", function(d) {
-      //   if (screen.width <= 480) {
-      //     return bubbles_tooltip
-      //       .style("top", (d3.event.pageY+20)+"px")
-      //       .style("left",d3.event.pageX/2+20+"px");
-      //   } else {
-      //     return bubbles_tooltip
-      //       .style("top", (d3.event.pageY+20)+"px")
-      //       .style("left",(d3.event.pageX-80)+"px");
-      //   }
-      // })
-      // .on("mouseout", function(){return bubbles_tooltip.style("visibility", "hidden");});
-
-  //create the bubbles
-  bubbles.append("circle")
-      .attr("r", function(d){ return d.r; })
-      .attr("cx", function(d){ return d.x; })
-      .attr("cy", function(d){ return d.y; })
-      .style("opacity",function(d) {
-        console.log(d);
-        // return "0.7";
-        if (d.call_types == highlight) {
-          return 0.8;
-        } else {
-          return 0.6;
-        }
-      })
-      .style("fill", function(d) {
-        return "#8E0000";
-      });
-
-  //format the text for each bubble
-  bubbles.append("text")
-      .attr("x", function(d){
-        if (d.call_types == "Runaway") {
-          return d.x;
-        } else {
-          return d.x;
-        }
-      })
-      .attr("y", function(d){ return d.y + 5; })
-      .attr("text-anchor", "middle")
-      .text(function(d){
-        if (d.call_types == highlight) {
-          return d["call_types"];
-        } else {
-          return "";
-        }
-      })
-      .style("fill","black")
-      .style("font-size","18px")
 
 }
 
@@ -693,38 +394,9 @@ var drawIcons = function(html_str,key) {
 
 
 // ------------------------------------------------------------------------------------------------------------------------
+drawDots("dots");
 
-
-// slides.forEach( function(slide) {
-
-//   console.log(slide["Interactive"]);
-
-//   // this is the map with numbers of kids on it
-//   if (slide["Interactive"] == "map"){
-//     drawMap("map","count");
-
-//   // this is the interactive that shows how Mary Graham is an outlier
-//   } else if (slide["Interactive"] == "bars"){
-//     drawBars();
-
-//   // this is the interactive that shows how Mary Graham is an outlier
-//   } else if (slide["Interactive"] == "dots"){
-    drawDots("dots");
-
-//   // this is the interactive that shows how Mary Graham is an outlier
-//   } else if (slide["Interactive"] == "bubbles"){
-//     drawBubbles("bubbles",0,"Runaway");
-
-//   // this is the icon chart that shows all the cases that were thrown out
-//   } else if ((slide["Interactive"] == "icons-arrests")) {
-
-//     var key = "icon-"+slide["Interactive"].split("-")[1];
-
-    // var html_str = drawIcons("",key);
-//     document.getElementById(slide["Interactive"]).innerHTML = html_str;
-
-//   } else if ((screen.width <= 480) && ((slide["Interactive"] == "icons-bookings") || (slide["Interactive"] == "icons-pursued") || (slide["Interactive"] == "icons-probation"))) {
-//     var key = "icon-"+slide["Interactive"].split("-")[1];
+drawMap("map","count");
 
 if (screen.width <= 480) {
   ["icons-arrests","icons-bookings","icons-pursued","icons-probation"].forEach(function(icon){
@@ -737,13 +409,6 @@ if (screen.width <= 480) {
   var html_str = drawIcons("",'arrests');
   document.getElementById('icons-arrests').innerHTML = html_str;
 }
-
-//   }
-
-// });
-
-drawMap("map","count");
-
 
 var prevIDX = -1;
 var icons_list = ["icons-arrests","icons-bookings","icons-pursued","icons-probation"];
@@ -775,8 +440,6 @@ $(window).scroll(function () {
       sticker_ph.style.display = 'none';
     }
   }
-
-//   console.log(currentIDX);
 
   if (currentIDX != prevIDX) {
     current_icons = icons_list[currentIDX];
